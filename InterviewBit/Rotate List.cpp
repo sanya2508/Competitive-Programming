40ecdf74b05416d27774ac4f98fc45bf4c709d3f
Sanya Kumari <55986045+sanya2508@users.noreply.github.com>
2020-02-18 19:28:33 +0530

    Create Rotate List.cpp

---
+++ InterviewBit/Rotate List.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::rotateRight(ListNode* A, int B) {
+     int n=1;
+    ListNode *tail=A;
+    ListNode *current=A;
+    while(tail->next){
+        n++;
+        tail=tail->next;
+    }
+    if(B>=n)
+        B%=n;
+    if(B==0)
+        return A;
+    n=n-B-1;
+    while(n--)
+        current=current->next;
+    tail->next=A;
+    A=current->next;
+    current->next=NULL;
+    return A;
+}
