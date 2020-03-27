664553ae33c3bb89821047bbba97bbf1571ccaef
Sanya Kumari <55986045+sanya2508@users.noreply.github.com>
2020-03-27 22:42:34 +0530

    Add files via upload

---
+++ Other/ramdomequation.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+ int max;
+   srand(unsigned(time(NULL)));
+   
+   double a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x;
+   float y1= 0.89608802455664;
+   float y2=-0.083381608939051;
+   float y3= 0.24686007793153;
+   float y4= 0;
+   float y5= 0.067658648473815;
+   float y6=-0.21072103131565;
+   float y7=0;
+   float y8=0.048790164169432;
+   float y9=0.13976194237516;
+   float y10=0.17395330712344;
+   float y11=0.13976194237516;
+   float y12=0;
+   float y13=0.095310179804325;
+   float y14=0.086177696241052;
+   float y15=0;
+   float y16=0.086177696241052;
+   float y17=-0.072570692834835;
+   float y18=0;
+   float y19=0.22020940501452;
+   float y20=0.17980808272355;
+   float y21=0.25084337466372;
+   float y22=0.14606631905197;
+   float y23=0.34629704820832;
+   float y24=4.4841028037230;
+   float CONSTVAL=5.94017125272043;
+   
+   
+for(int i=0; i<INT_MAX; i++){
+	a=(double)rand()/(RAND_MAX);
+	b=(double)rand()/(RAND_MAX);
+	c=(double)rand()/(RAND_MAX);
+	d=(double)rand()/(RAND_MAX);
+	e=(double)rand()/(RAND_MAX);
+	f=(double)rand()/(RAND_MAX);
+	g=(double)rand()/(RAND_MAX);
+	h=(double)rand()/(RAND_MAX);
+	i=(double)rand()/(RAND_MAX);
+	j=(double)rand()/(RAND_MAX);
+	k=(double)rand()/(RAND_MAX);
+	l=(double)rand()/(RAND_MAX);
+	m=(double)rand()/(RAND_MAX);
+	n=(double)rand()/(RAND_MAX);
+	o=(double)rand()/(RAND_MAX);
+	p=(double)rand()/(RAND_MAX);
+	q=(double)rand()/(RAND_MAX);
+	r=(double)rand()/(RAND_MAX);
+	s=(double)rand()/(RAND_MAX);
+	t=(double)rand()/(RAND_MAX);
+	u=(double)rand()/(RAND_MAX);
+	v=(double)rand()/(RAND_MAX);
+	w=(double)rand()/(RAND_MAX);
+	x=(double)rand()/(RAND_MAX);
+   	if(((a*y1)+(b*y2)+(c*y3)+(d*y4)+(e*y5)+(f*y6)+(g*y7)+(h*y8)+(i*y9)+(j*y10)+(k*y11)+(l*y12)+(m*y13)+(n*y14)+(o*y15)+(p*y16)+(q*y17)+(r*y18)+(s*y19)+(t*y20)+(u*y21)+(v*y22)+(w*y23)+(x*y24)) ==CONSTVAL)
+   	{
+   		cout<<"a = "<<a<<endl;
+   		cout<<"b = "<<b<<endl;
+   		cout<<"c = "<<c<<endl;
+   		cout<<"d = "<<d<<endl;
+   		cout<<"e = "<<e<<endl;
+   		cout<<"f = "<<f<<endl;
+   		cout<<"g = "<<g<<endl;
+   		cout<<"h = "<<h<<endl;
+   		cout<<"i = "<<i<<endl;
+   		cout<<"j = "<<j<<endl;
+   		cout<<"k = "<<k<<endl;
+   		cout<<"l = "<<l<<endl;
+   		cout<<"m = "<<m<<endl;
+   		cout<<"n = "<<n<<endl;
+   		cout<<"o = "<<o<<endl;
+   		cout<<"p = "<<p<<endl;
+   		cout<<"q = "<<q<<endl;
+   		cout<<"r = "<<r<<endl;
+   		cout<<"s = "<<s<<endl;
+   		cout<<"t = "<<t<<endl;
+   		cout<<"u = "<<u<<endl;
+   		cout<<"v = "<<v<<endl;
+   		cout<<"w = "<<w<<endl;
+   		cout<<"x = "<<x<<endl;
+   		break;
+   	}
+}
+}
