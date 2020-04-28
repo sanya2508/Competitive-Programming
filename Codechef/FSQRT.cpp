6018c13fab74c4bd065ed77aff579b534dab3895
Sanya Kumari <55986045+sanya2508@users.noreply.github.com>
2020-04-28 15:59:14 +0530

    Create FSQRT.cpp

---
+++ Codechef/FSQRT.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	  int n,x;
+	    cin>>n;
+	    x=int(sqrt(n));
+	    cout<<x<<endl;
+	    
+	}
+	
+	return 0;
+}
