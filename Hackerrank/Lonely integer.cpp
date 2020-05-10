f9209ac84deba97a882eebd612a4cfa77a43a08a
Sanya Kumari <1701sanya@gmail.com>
2020-05-10 16:41:33 +0530

    Create Lonely integer.cpp

---
+++ Hackerrank/Lonely integer.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+using namespace std; 
+  
+int findSingle(int ar[], int ar_size) 
+    { 
+        int res = ar[0]; 
+        for (int i = 1; i < ar_size; i++) 
+            res = res ^ ar[i]; 
+  
+        return res; 
+    } 
+  
+int main() 
+    {     int arr[100000];
+    int n;
+    cin>>n;
+    for(int i=0; i<n;i++)
+    {
+        cin>>arr[i];
+        }
+        cout<< findSingle(arr, n); 
+        return 0; 
+    } 
