b0c15f8d2face3ee855da8b272a9b300d319d8e8
Sanya Kumari <1701sanya@gmail.com>
2020-08-04 23:18:28 +0530

    Create Copy List with Random Pointer.cpp

---
+++ LeetCode/Copy List with Random Pointer.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head) return head;
+        unordered_map<Node*,Node*> m;
+        Node* tmp = head;
+        while(tmp){
+            m[tmp] = new Node(tmp->val);
+            tmp = tmp->next;
+        }
+        tmp = head;
+        while(tmp){
+            m[tmp]->next = m[tmp->next];
+            m[tmp]->random = m[tmp->random];
+            tmp = tmp->next;
+        }
+        return m[head];
+    }
+};
