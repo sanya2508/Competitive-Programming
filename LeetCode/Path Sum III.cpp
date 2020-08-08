23ebb01886e7fbbe74a878c0f828810732ead69c
Sanya Kumari <1701sanya@gmail.com>
2020-08-09 01:21:24 +0530

    August: Day 8

---
+++ LeetCode/Path Sum III.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int pathSum_a(TreeNode* root, int sum){
+        if(root == nullptr) return 0;
+        int res = 0;
+        if(root->val == sum) res++;
+        res += pathSum_a(root->left, sum-root->val);
+        res += pathSum_a(root->right, sum-root->val);
+        return res;
+    }
+public:
+    int pathSum(TreeNode* root, int sum) {
+        if(root == nullptr) return 0;
+        return pathSum(root->left, sum) + pathSum_a(root, sum) + pathSum(root->right, sum);
+    }
+};
