9be73d17d2bdfe90eeea56f56a97c87b03878cd5
Sanya Kumari <1701sanya@gmail.com>
2020-08-11 23:27:08 +0530

    August: Day 11

--- LeetCode/H-Index.cpp
+++ LeetCode/H-Index.cpp
@@ -1,1 +1,10 @@
-//
+class Solution {
+public: 
+    int hIndex(vector<int>& citations) {
+        sort(citations.rbegin(), citations.rend());
+        int ans = 0;
+        for(int i = 0; i < citations.size(); i++)
+            if(citations[i]>i) ans++;        
+        return ans;        
+    }
+};
