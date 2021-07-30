9a4912597c79a4779264dc5207bebce260594990
Sanya <1701sanya@gmail.com>
2021-07-31 02:41:22 +0530

    Create B1. Two Sum.cpp

---
+++ LeetCode/B1. Two Sum.cpp
@@ -0,0 +1,32 @@
+// O(n2)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i=0; i<nums.size()-1; i++){
+            for(int j=i+1; j<nums.size(); j++)
+                if(nums[i]+nums[j]==target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+        }
+        return ans;
+    }
+};
+
+// O(n) -> One pass
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        for(int i=0; i<nums.size(); i++){
+            if(m.find(target-nums[i]) != m.end()){
+                return {m[target-nums[i]], i};
+            }
+            m[nums[i]]=i;
+        }
+        return {};
+    }
+};
